1088cf2b21f7b351475477292dbfd5009a742baa
geodk00 <tor@leeberg.dk>
2021-03-08 23:36:53 +0100

    feat: add comments and tidy up LogoutView

--- src/views/LogoutView.js
+++ src/views/LogoutView.js
@@ -4,10 +4,21 @@ import { removeState } from '../utils/localstorage'
 
 import ProtectedComponent from '../components/ProtectedComponent'
 
+/*
+  View responsible for logging the user out.
+  Will delete saved state from localStorage and
+  reset the core application state.
+
+  Resetting the user will make the app redirect the
+  user to the login page automatically
+*/
+
 const LogoutView = (props) => {
+  /* PROPS */
   const { changeUser } = props
   const { resetTranslations } = props
 
+  /* LIFECYCLE */
   useEffect(() => {
     removeState('user')
     removeState('translations')
@@ -18,11 +29,13 @@ const LogoutView = (props) => {
   return (
     <div>
       <p>You have been logged out</p>
-
     </div>
   )
 }
 
-LogoutView.propTypes = { changeUser: PropTypes.func.isRequired }
-LogoutView.propTypes = { resetTranslations: PropTypes.func.isRequired }
+LogoutView.propTypes = {
+  changeUser: PropTypes.func.isRequired,
+  resetTranslations: PropTypes.func.isRequired
+}
+
 export default ProtectedComponent(LogoutView)
